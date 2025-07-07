30797c5a314acf6fa29fc9ff88d89a6c91c9a400
DeepthiVibinraj <deepthivibinraj@gmail.com>
2025-07-07 22:15:14 +0530

    server

--- server.js
+++ server.js
@@ -1,3 +1,64 @@
+// const { ApolloServer } = require("apollo-server");
+// const dotenv = require("dotenv");
+
+// const connectDB = require("./config/db");
+// const studentTypeDef = require("./graphql/typedefs/studentTypeDef");
+// const userTypeDef = require("./graphql/typedefs/userTypeDef");
+// const studentResolver = require("./graphql/resolvers/studentResolver");
+// const userResolver = require("./graphql/resolvers/userResolver");
+
+// const staffTypeDef = require("./graphql/typedefs/staffTypeDef");
+
+// const staffResolver = require("./graphql/resolvers/staffResolver");
+
+// const subjectTypeDef = require("./graphql/typedefs/subjectTypeDef");
+
+// const subjectResolver = require("./graphql/resolvers/subjectResolver");
+// const classTypeDef = require("./graphql/typedefs/classTypeDef");
+
+// const classResolver = require("./graphql/resolvers/classResolver");
+// const timeTableTypeDef = require("./graphql/typedefs/timeTableTypeDef");
+
+// const timeTableResolver = require("./graphql/resolvers/timeTableResolver");
+
+// const typeDefs = [
+//   studentTypeDef,
+//   userTypeDef,
+//   staffTypeDef,
+//   subjectTypeDef,
+//   classTypeDef,
+//   timeTableTypeDef,
+// ];
+// const resolvers = [
+//   studentResolver,
+//   userResolver,
+//   staffResolver,
+//   subjectResolver,
+//   classResolver,
+//   timeTableResolver,
+// ];
+
+// // Load environment variables
+// dotenv.config();
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Create Apollo Server
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   cache: "bounded",
+//   context: ({ req }) => {
+//     // Add any authentication logic here if needed
+//     return { req };
+//   },
+// });
+
+// // Start the server
+// server.listen({ port: process.env.PORT || 10000 }).then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
 const { ApolloServer } = require("apollo-server");
 const dotenv = require("dotenv");
 
@@ -6,20 +67,20 @@ const studentTypeDef = require("./graphql/typedefs/studentTypeDef");
 const userTypeDef = require("./graphql/typedefs/userTypeDef");
 const studentResolver = require("./graphql/resolvers/studentResolver");
 const userResolver = require("./graphql/resolvers/userResolver");
-
 const staffTypeDef = require("./graphql/typedefs/staffTypeDef");
-
 const staffResolver = require("./graphql/resolvers/staffResolver");
-
 const subjectTypeDef = require("./graphql/typedefs/subjectTypeDef");
-
 const subjectResolver = require("./graphql/resolvers/subjectResolver");
 const classTypeDef = require("./graphql/typedefs/classTypeDef");
-
 const classResolver = require("./graphql/resolvers/classResolver");
 const timeTableTypeDef = require("./graphql/typedefs/timeTableTypeDef");
-
 const timeTableResolver = require("./graphql/resolvers/timeTableResolver");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const typeDefs = [
   studentTypeDef,
@@ -29,6 +90,7 @@ const typeDefs = [
   classTypeDef,
   timeTableTypeDef,
 ];
+
 const resolvers = [
   studentResolver,
   userResolver,
@@ -38,20 +100,17 @@ const resolvers = [
   timeTableResolver,
 ];
 
-// Load environment variables
-dotenv.config();
-
-// Connect to MongoDB
-connectDB();
-
 // Create Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   cache: "bounded",
   context: ({ req }) => {
-    // Add any authentication logic here if needed
     return { req };
+  },
+  cors: {
+    origin: "*", // Allow all origins (for development only)
+    credentials: true,
   },
 });
 
