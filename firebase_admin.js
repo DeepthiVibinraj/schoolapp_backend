91bf0facc47626ec7704cdf18e8bc981b2af8022
DeepthiVibinraj <deepthivibinraj@gmail.com>
2025-07-17 10:07:58 +0530

    service_account fix

--- firebase_admin.js
+++ firebase_admin.js
@@ -53,7 +53,10 @@
 
 // module.exports = admin;// firebase_admin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json");
+// const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString()
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
